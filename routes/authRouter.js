70ab7245c742d0f7b0c513d2ec9a86c618704567
pasipasipada <irfanoputra27@gmail.com>
2024-11-02 22:54:04 +0700

    feat : add file routes for auth

---
+++ routes/authRouter.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const authController = require("../controllers/authController");
+
+router.get("", authController.findUsers);
+router.post("/login", authController.login);
+router.post("/register", authController.register);
+
+module.exports = router;
