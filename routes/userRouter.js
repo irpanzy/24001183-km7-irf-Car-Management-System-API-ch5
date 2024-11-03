e4de0dbb95fe4375d2ed0f195163ac347c438a49
pasipasipada <irfanoputra27@gmail.com>
2024-11-03 11:51:22 +0700

    feat : add authenticate users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,10 +4,10 @@ const authenticate = require("../middlewares/authenticate");
 const userController = require("../controllers/userController");
 
 router.post("", authenticate(["superadmin"]), userController.createAdmin);
-router.get("", userController.findUsers);
+router.get("", authenticate(["admin", "superadmin", "member"]), userController.findUsers);
 router.get("/current-user", authenticate(["admin", "superadmin", "member"]), userController.getCurrentUser);
-router.get("/:id", userController.findUserById);
-router.patch("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get("/:id", authenticate(["admin", "superadmin", "member"]), userController.findUserById);
+router.patch("/:id", authenticate(["admin", "superadmin", "member"]), userController.updateUser);
+router.delete("/:id", authenticate(["admin", "superadmin", "member"]), userController.deleteUser);
 
 module.exports = router;
